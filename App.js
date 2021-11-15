39d7f71dabbe5427fe72aebc9cd11a5cade39555
LiYiran <e0674567@u.nus.edu>
2021-11-15 21:25:10 +0800

    add refresh functionality, add table head, change font size

--- App.js
+++ App.js
@@ -3,26 +3,73 @@ import { Button, View, Text, TextInput, StyleSheet, Alert, SafeAreaView, ScrollV
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider, graphql, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import RNRestart from 'react-native-restart';
 
 styles = StyleSheet.create({
   welcome: {
     fontSize: 18,
     textAlign: 'center',
     margin: 10,
-      marginTop:20
+      marginTop:20,
+      fontWeight: "bold"
   },
   tableCont: {
       fontSize: 18,
       flexDirection:"row", 
       justifyContent: 'space-around',
-    margin: 10
+    margin: 10,
+      marginRight:20
+  },
+    tableHead: {
+      fontSize: 18,
+      flexDirection:"row", 
+      justifyContent: 'space-around',
+      marginRight:20,
+        marginLeft:10
   },
     container: {
         marginTop: 50
     },
     head: {
         fontSize: 20,
+        textAlign: 'center',
+        fontWeight: "bold"
+    },
+    serialno: {
+        width: 60,
         textAlign: 'center'
+    },
+    name: {
+        width: 50,
+        textAlign: 'center'
+    },
+    phone: {
+        width: 65,
+        textAlign: 'center'
+    },
+    time: {
+        width: 175,
+        textAlign: 'center'
+    },
+    serialno_head: {
+        width: 60,
+        textAlign: 'center',
+        fontWeight: "bold"
+    },
+    name_head: {
+        width: 50,
+        textAlign: 'center',
+        fontWeight: "bold"
+    },
+    phone_head: {
+        width: 65,
+        textAlign: 'center',
+        fontWeight: "bold"
+    },
+    time_head: {
+        width: 175,
+        textAlign: 'center',
+        fontWeight: "bold"
     }
 })
 
@@ -46,10 +93,10 @@ const IssueComponent = graphql(dogQuery)(props => {
       <View>
         {issueList.map(issue => {
           return <View style={styles.tableCont} key={issue.SerialNo}>
-                    <Text style={styles.eachItem}>{issue.SerialNo}</Text>
-                    <Text style={styles.eachItem}>{issue.Name}</Text>
-                    <Text style={styles.eachItem}>{issue.Phone}</Text>
-                    <Text style={styles.eachItem}>{issue.Timestamp}</Text>
+                    <Text style={styles.serialno}>{issue.SerialNo}</Text>
+                    <Text style={styles.name}>{issue.Name}</Text>
+                    <Text style={styles.phone}>{issue.Phone}</Text>
+                    <Text style={styles.time}>{issue.Timestamp}</Text>
                 </View>;
         })}
       </View>
@@ -60,13 +107,24 @@ const IssueComponent = graphql(dogQuery)(props => {
 });
 
 export default class App extends Component {
+  refresh() {
+      RNRestart.Restart()
+  }
+    
   render() {
     return (
       <ApolloProvider client={client}>
         <ScrollView style={styles.container}>
             <Text style={styles.head}>Hotel California Waitlist System</Text>
             <Text style={styles.welcome}>Current Waitlist:</Text>
+            <View style={styles.tableHead}>
+                <Text style={styles.serialno_head}>SerialNo</Text>
+                <Text style={styles.name_head}>Name</Text>
+                <Text style={styles.phone_head}>Phone</Text>
+                <Text style={styles.time_head}>Timestamp</Text>
+            </View>
             <IssueComponent />
+            <Button style={styles.welcome} title="Refresh" onPress={this.refresh} />
         </ScrollView>
       </ApolloProvider>
     );
